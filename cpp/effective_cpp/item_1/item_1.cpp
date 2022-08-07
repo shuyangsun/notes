2dd3d2d6df6f92baa4e83fa47dcfb555bb57ca28
Shuyang Sun <shuyangsun10@gmail.com>
2022-08-07 17:42:33 -0400

    More examples of item_1.

--- cpp/effective_cpp/item_1/item_1.cpp
+++ cpp/effective_cpp/item_1/item_1.cpp
@@ -13,6 +13,11 @@ void f_ref(T& param) {
 }
 
 template<typename T>
+void f_cref(const T& param) {
+  PRINT_DEDUCED_TYPE();
+}
+
+template<typename T>
 void f_ptr(T* param) {
   PRINT_DEDUCED_TYPE();
 }
@@ -35,15 +40,23 @@ int main(int argc, char **argv) {
   int x = 27;
   const int cx = x;
   const int& rx = x;
-  const int* px = &x;
+  int const * const px = &x;
 
   std::cout << "Case 1:" << std::endl;
 
   f_ref(x);    // ParamType: int&, T: int
   f_ref(cx);   // ParamType: int const&, T: int const
   f_ref(rx);   // ParamType: int const&, T: int const
-  f_ref(px);   // ParamType: int const*&, T: int const*
+  f_ref(px);   // ParamType: int const* const&, T: int const* const
   // f_ref(27);   // Does not compile
+
+  std::cout << std::endl;
+
+  f_cref(x);    // ParamType: int const&, T: int
+  f_cref(cx);   // ParamType: int const&, T: int
+  f_cref(rx);   // ParamType: int const&, T: int
+  f_cref(px);   // ParamType: int const* const&, T: int const*
+  f_cref(27);   // int const&, T: int
 
   std::cout << std::endl;
 
@@ -59,7 +72,7 @@ int main(int argc, char **argv) {
   f_uref(x);   // ParamType: int&, T: int&
   f_uref(cx);  // ParamType: int const&, T: int const&
   f_uref(rx);  // ParamType: int const&, T: int const&
-  f_uref(px);  // ParamType: int const*&, T: int const*&
+  f_uref(px);  // ParamType: int const* const&, T: int const* const&
   f_uref(27);  // ParamType: int&&, T: int
 
   std::cout << std::endl;

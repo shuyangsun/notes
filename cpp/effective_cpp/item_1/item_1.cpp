6ac2d3943f9142df3dd4ad94e6f98e5e8cebf277
Shuyang Sun <shuyangsun10@gmail.com>
2022-08-08 12:44:44 -0400

    Function type deduction.

--- cpp/effective_cpp/item_1/item_1.cpp
+++ cpp/effective_cpp/item_1/item_1.cpp
@@ -41,35 +41,51 @@ void f_cbyval(const T param) {
   PRINT_DEDUCED_TYPE();
 }
 
+template<typename T, std::size_t N>
+constexpr std::size_t ArraySize(T (&)[N] ) noexcept {
+  return N;
+}
+
+void MyFunc(int, double) {}
+
 int main(int argc, char **argv) {
   int x = 27;
   const int cx = x;
   const int& rx = x;
-  int const * const px = &x;
+  char ch = 'a';
+  char const * const pc = &ch;
+  const char ac[] = "Effective C++ Notes";
 
   std::cout << "Case 1:" << std::endl;
 
   f_ref(x);    // ParamType: int&, T: int
   f_ref(cx);   // ParamType: int const&, T: int const
   f_ref(rx);   // ParamType: int const&, T: int const
-  f_ref(px);   // ParamType: int const* const&, T: int const* const
   // f_ref(27);   // Does not compile
+  f_ref(pc);   // ParamType: char const* const&, T: char const* const
+  f_ref(ac);   // ParamType: char const (&) [20], T: char const [20]
+  f_ref(MyFunc);   // ParamType: void (&)(int, double), T: void (int, double)
 
   std::cout << std::endl;
 
   f_cref(x);    // ParamType: int const&, T: int
   f_cref(cx);   // ParamType: int const&, T: int
   f_cref(rx);   // ParamType: int const&, T: int
-  f_cref(px);   // ParamType: int const* const&, T: int const*
   f_cref(27);   // int const&, T: int
+  f_cref(pc);   // ParamType: char const* const&, T: char const*
+  f_cref(ac);   // ParamType: char const (&) [20], T: char [20]
+  f_cref(MyFunc);   // ParamType: void (&)(int, double), T: void (int, double)
 
   std::cout << std::endl;
 
   f_ptr(&x);   // ParamType: int*, T: int
   f_ptr(&cx);  // ParamType: int const*, T: int const
   f_ptr(&rx);  // ParamType: int const*, T: int const
-  f_ptr(px);   // ParamType: int const*, T: int const
   // f_ptr(27);   // Does not compile
+  // f_ptr(&27);  // Does not compile
+  f_ptr(pc);   // ParamType: char const*, T: char const
+  f_ptr(ac);   // ParamType: char const*, T: char const
+  f_ptr(MyFunc);   // ParamType: void (*)(int, double), T: void (int, double)
 
   std::cout << std::endl;
   std::cout << "Case 2:" << std::endl;
@@ -77,8 +93,10 @@ int main(int argc, char **argv) {
   f_uref(x);   // ParamType: int&, T: int&
   f_uref(cx);  // ParamType: int const&, T: int const&
   f_uref(rx);  // ParamType: int const&, T: int const&
-  f_uref(px);  // ParamType: int const* const&, T: int const* const&
   f_uref(27);  // ParamType: int&&, T: int
+  f_uref(pc);  // ParamType: char const* const&, T: char const* const&
+  f_uref(ac);  // ParamType: char const (&) [20], T: char const (&) [20]
+  f_uref(MyFunc);  // ParamType: void (&)(int, double), T: void (&)(int, double)
 
   std::cout << std::endl;
   std::cout << "Case 3:" << std::endl;
@@ -86,16 +104,24 @@ int main(int argc, char **argv) {
   f_byval(x);  // ParamType: int, T: int
   f_byval(cx); // ParamType: int, T: int
   f_byval(rx); // ParamType: int, T: int
-  f_byval(px); // ParamType: int const*, T: int const*
   f_byval(27); // ParamType: int, T: int
+  f_byval(pc); // ParamType: char const*, T: char const*
+  f_byval(ac); // ParamType: char const*, T: char const*
+  f_byval(MyFunc); // ParamType: void (*)(int, double), T: void (*)(int, double)
 
   std::cout << std::endl;
 
   f_cbyval(x);  // ParamType: int const, T: int
   f_cbyval(cx); // ParamType: int const, T: int
   f_cbyval(rx); // ParamType: int const, T: int
-  f_cbyval(px); // ParamType: int const* const, T: int const*
   f_cbyval(27); // ParamType: int const, T: int
+  f_cbyval(pc); // ParamType: char const* const, T: char const*
+  f_cbyval(ac); // ParamType: char const* const, T: char const*
+  f_cbyval(MyFunc); // ParamType: void (* const)(int, double), T: void (*)(int, double)
+
+  std::cout << std::endl;
+  std::cout << "Array size deduction:" << std::endl;
+  std::cout << "Size of char array \"" << ac << "\" is " << ArraySize(ac) << std::endl;
 
   return 0;
 }

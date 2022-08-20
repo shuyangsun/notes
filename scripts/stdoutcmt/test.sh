95461a9839168223387bebb106061fca1c1e479a
Shuyang Sun <shuyangsun10@gmail.com>
2022-08-20 15:45:01 -0400

    Change test script.

--- scripts/stdoutcmt/test.sh
+++ scripts/stdoutcmt/test.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 "$SCRIPT_DIR/build.sh" -DTESTS=ON && \
-"$SCRIPT_DIR/build/unit_test"
+ctest --extra-verbose --test-dir "$SCRIPT_DIR/build"

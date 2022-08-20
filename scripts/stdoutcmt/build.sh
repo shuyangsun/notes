065d984799c6e5398fb75ff9af4ab8cac72951cc
Shuyang Sun <shuyangsun10@gmail.com>
2022-08-20 10:04:00 -0400

    Do not build test case for executable.

--- scripts/stdoutcmt/build.sh
+++ scripts/stdoutcmt/build.sh
@@ -4,5 +4,7 @@ SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 mkdir -p "$SCRIPT_DIR"/build
 cd "$SCRIPT_DIR"/build
-cmake -DCMAKE_BUILD_TYPE=Release "$SCRIPT_DIR"
+
+cmake "${@:1}" -DCMAKE_BUILD_TYPE=Release "$SCRIPT_DIR"
+
 make

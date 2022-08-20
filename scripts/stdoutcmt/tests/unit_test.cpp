94edacaa81031304de11fba66f047a7bb25c40bd
Shuyang Sun <shuyangsun10@gmail.com>
2022-08-20 14:50:38 -0400

    Move more tests to files.

--- scripts/stdoutcmt/tests/unit_test.cpp
+++ scripts/stdoutcmt/tests/unit_test.cpp
@@ -84,36 +84,26 @@ TEST(Utility, CommentRemoval_2) {
   EXPECT_STREQ(res.c_str(), "  ");
 }
 
-TEST(SrcParser, cpp_001) {
-  const std::string input{util::Read(GetDataPath("src_parsing/cpp_001_input.txt"))};
-  std::ifstream out_file(GetDataPath("src_parsing/cpp_001_output.json"));
-  json data = json::parse(out_file);
-
-  const src::LineOffsetMap res{SRC_PARSER_CPP->GetCmtLineOffset(util::ToLines(input))};
-  EXPECT_EQ(res.size(), data["cmt_offset"].size());
-  for (auto&& [line, offset]: data["cmt_offset"].items()) {
-    const uint64_t line_num{util::FromStr<uint64_t>(line).value()};
-    EXPECT_TRUE(res.find(line_num) != res.end());
-    EXPECT_EQ(res.at(line_num), offset);
-  }
+#define SRC_PARSER_TEST(TEST_NAME); \
+TEST(SrcParser, TEST_NAME) {        \
+  const std::string test_name{#TEST_NAME}; \
+  const std::string input{util::Read(GetDataPath("src_parsing/" + test_name + "_input.txt"))}; \
+  std::ifstream out_file(GetDataPath("src_parsing/" + test_name + "_output.json")); \
+  json data = json::parse(out_file); \
+  const src::LineOffsetMap res{SRC_PARSER_CPP->GetCmtLineOffset(util::ToLines(input))}; \
+  EXPECT_EQ(res.size(), data["cmt_offset"].size()); \
+  for (auto&& [line, offset]: data["cmt_offset"].items()) { \
+    const uint64_t line_num{util::FromStr<uint64_t>(line).value()}; \
+    const int64_t offset_num{offset.get<int64_t>()}; \
+    const std::string err_msg{"Expecting line " + std::to_string(line_num) + " to have offset " + std::to_string(offset_num) + "."}; \
+    EXPECT_TRUE(res.find(line_num) != res.end()) << err_msg; \
+    EXPECT_EQ(res.at(line_num), offset_num) << err_msg; \
+  } \
 }
 
-TEST(SrcParser, Cpp_3) {
-  const std::vector<std::string_view> lines{
-      "#include <iostream>",
-      "",
-      "int main(int argc, char** argv) {",
-      "  #pragma cmt beg",
-      "  const int x{0}; std::cout << x << std::endl;",
-      "  #pragma cmt end",
-      "  return 0;",
-      "}",
-  };
-
-  const src::LineOffsetMap res{SRC_PARSER_CPP->GetCmtLineOffset(lines)};
-  EXPECT_EQ(res.size(), 1);
-  EXPECT_EQ(res.at(4), 0);
-}
+SRC_PARSER_TEST(cpp_001);
+SRC_PARSER_TEST(cpp_002);
+SRC_PARSER_TEST(cpp_003);
 
 TEST(SrcParser, Cpp_4) {
   const std::vector<std::string_view> lines{

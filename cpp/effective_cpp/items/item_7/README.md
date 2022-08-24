d474072ba0a2e4dc369206df08acf062d1e3228d
Shuyang Sun <shuyangsun10@gmail.com>
2022-08-24 15:41:01 -0400

    Add Widget class example.

--- cpp/effective_cpp/items/item_7/README.md
+++ cpp/effective_cpp/items/item_7/README.md
@@ -6,4 +6,12 @@ Three main ways to initialize a variable:
 2. Parentheses: `int x(1);`
 3. Braces: `int x{1};`
 
-(You can also use equal sign and braces: `int x = {1};`.)
+(You can also use equal sign and braces: `int x = {1};`, usually it's treated the same as the brace syntax.)
+
+The differences are mainly for user-defined types:
+
+```c++
+Widgeet w1;  // calls default constructor: Widget()
+Widget w2 = w1;  // calls copy constructor: Widget(const Widget& other)
+w1 = w2;  // calls copy assignment: Widget& operator=(const Widget& rhs)
+```
--- cpp/effective_cpp/items/item_7/item_7.cpp
+++ cpp/effective_cpp/items/item_7/item_7.cpp
@@ -3,6 +3,22 @@
 
 #include "cpp_notes/util/print.h"
 
+class Widget {
+ public:
+  Widget() {
+    std::cout << "calling default constructor..." << std::endl;
+  }
+
+  Widget(const Widget& other) {
+    std::cout << "calling copy constructor..." << std::endl;
+  }
+
+  Widget& operator=(const Widget& rhs) {
+    std::cout << "calling copy assignment operator..." << std::endl;
+    return *this;
+  }
+};
+
 int main(int argc, char **argv) {
   #pragma cmt beg
   int x = 1;
@@ -15,6 +31,11 @@ int main(int argc, char **argv) {
 
   std::initializer_list<int> u{1, 2};
   std::initializer_list<int> v = {1, 2};
+
+  Widget w1;      //> calling default constructor...
+  Widget w2 = w1; //> calling copy constructor...
+  w1 = w2;        //> calling copy assignment operator...
+
   #pragma cmt end
   return 0;
 }

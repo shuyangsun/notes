9e1fdd3d762296c9b9d776bcfa315596f012d350
Shuyang Sun <shuyangsun10@gmail.com>
2022-08-24 15:26:17 -0400

    Comments change for item_7.

--- cpp/effective_cpp/items/item_7/item_7.cpp
+++ cpp/effective_cpp/items/item_7/item_7.cpp
@@ -10,8 +10,8 @@ int main(int argc, char **argv) {
   int z{1};
 
   int p = {1};
-  auto q{1}; PRINT_TYPE(q);
-  auto r = {1}; PRINT_TYPE(r);
+  auto q{1}; PRINT_TYPE(q);    //> int
+  auto r = {1}; PRINT_TYPE(r); //> std::initializer_list<int>
 
   std::initializer_list<int> u{1, 2};
   std::initializer_list<int> v = {1, 2};

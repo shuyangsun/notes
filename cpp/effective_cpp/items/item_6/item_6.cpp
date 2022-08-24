5491cbd86f5ad13758a9b6951b0103b4932f5c9b
Shuyang Sun <shuyangsun10@gmail.com>
2022-08-24 14:31:18 -0400

    Add example for item_6.

--- cpp/effective_cpp/items/item_6/item_6.cpp
+++ cpp/effective_cpp/items/item_6/item_6.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <vector>
 
 #include "cpp_notes/util/print.h"
 #include "cpp_notes/util/bench.h"
 
 int main(int argc, char **argv) {
-
   #pragma cmt beg
-  #pragma cmt end
+  std::vector<bool> vec{true, false, true};
 
+  const auto ele_1{vec[1]}; PRINT_TYPE(ele_1); //> std::__1::__bit_reference<std::__1::vector<bool, std::__1::allocator<bool> >, true> const
+  const bool ele_2{vec[1]}; PRINT_TYPE(ele_2); //> bool const
+
+  #pragma cmt end
   return 0;
 }

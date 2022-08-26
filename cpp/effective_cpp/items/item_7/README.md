b03c42ccd0891049d23eec1c2701d044da8c4025
Shuyang Sun <shuyangsun10@gmail.com>
2022-08-26 18:32:24 -0400

    Item 7 notes change.

--- cpp/effective_cpp/items/item_7/README.md
+++ cpp/effective_cpp/items/item_7/README.md
@@ -10,10 +10,30 @@ Three main ways to initialize a variable:
 
 (You can also use equal sign and braces: `int x = {1};`, usually it's treated the same as the brace syntax.)
 
-The differences are mainly for user-defined types:
+## Key Differences
+
+1. Braces check for narrowing conversions.
+2. For user-defined types the difference could be significant.
 
 ```c++
 Widgeet w1;  // calls default constructor: Widget()
 Widget w2 = w1;  // calls copy constructor: Widget(const Widget& other)
 w1 = w2;  // calls copy assignment: Widget& operator=(const Widget& rhs)
 ```
+
+3. Braces and equal sign can also initialize non-static data members.
+
+```c++
+class Widget {
+  //...
+ private:
+  int x{0};
+  int y = 0;
+  // int z(0); // *** COMPILE ERROR ***
+};
+```
+
+4. `Widget w();` is parsed as function declaration instead of object construction. Only `Widget w;` or `Widget w{};` calls the default constructor.
+5. In constructor calls:
+   1. If no constructor declares a parameter of `std::initializer_list`: braces and parenthesis behave the same.
+   2. If one or more constructor declares a parameter of `std::initializer_list`: the overloading rule *strongly* prefers the initializer_list version, even when narrowing conversion is necessary.
--- cpp/effective_cpp/items/item_7/item_7.cpp
+++ cpp/effective_cpp/items/item_7/item_7.cpp
@@ -9,6 +9,14 @@ class Widget {
     std::cout << "calling default constructor..." << std::endl;
   }
 
+  Widget(int i, bool b) {
+    std::cout << "calling (int, bool) constructor..." << std::endl;
+  }
+
+  Widget(int i, double d) {
+    std::cout << "calling (int, double) constructor..." << std::endl;
+  }
+
   Widget(const Widget& other) {
     std::cout << "calling copy constructor..." << std::endl;
   }
@@ -16,6 +24,25 @@ class Widget {
   Widget& operator=(const Widget& rhs) {
     std::cout << "calling copy assignment operator..." << std::endl;
     return *this;
+  }
+};
+
+class WidgetWithInitList {
+ public:
+  WidgetWithInitList() {
+    std::cout << "calling default constructor..." << std::endl;
+  }
+
+  WidgetWithInitList(int i, bool b) {
+    std::cout << "calling (int, bool) constructor..." << std::endl;
+  }
+
+  WidgetWithInitList(int i, double d) {
+    std::cout << "calling (int, double) constructor..." << std::endl;
+  }
+
+  WidgetWithInitList(std::initializer_list<double> lst) {
+    std::cout << "calling (initializer_list<double>) constructor..." << std::endl;
   }
 };
 
@@ -36,6 +63,19 @@ int main(int argc, char **argv) {
   Widget w2 = w1; //> calling copy constructor...
   w1 = w2;        //> calling copy assignment operator...
 
+  Widget w3(1, true); //> calling (int, bool) constructor...
+  Widget w4{1, true}; //> calling (int, bool) constructor...
+  Widget w5(1, 0.5);  //> calling (int, double) constructor...
+  Widget w6{1, 0.5};  //> calling (int, double) constructor...
+
+  WidgetWithInitList w7(); // WARNING: Empty parentheses interpreted as a function declaration.
+  WidgetWithInitList w8{};           //> calling default constructor...
+  WidgetWithInitList w9(1, true);    //> calling (int, bool) constructor...
+  WidgetWithInitList w10{1, true};   //> calling (initializer_list<double>) constructor...
+  WidgetWithInitList w11(1, 0.5);    //> calling (int, double) constructor...
+  WidgetWithInitList w12{1, 0.5};    //> calling (initializer_list<double>) constructor...
+  WidgetWithInitList w13{0.5, 3.14}; //> calling (initializer_list<double>) constructor...
   #pragma cmt end
+
   return 0;
 }

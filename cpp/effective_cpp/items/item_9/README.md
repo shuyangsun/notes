e92d38d3b41d5db5700da7fb38e28dde4a989214
Shuyang Sun <shuyangsun10@gmail.com>
2022-09-11 13:52:19 -0400

    Add item_9.

--- cpp/effective_cpp/items/item_9/README.md
+++ cpp/effective_cpp/items/item_9/README.md
@@ -2,3 +2,22 @@
 
 # Item 9: Prefer alias declarations to `typedef`s.
 
+Alias declarations are easier to read and can be used with templates which `typedef`s cannot. Using types defined with `typedef`s inside of a template requires prefixing with `typename` (it is a *dependent type*), this is not the case for alias declarations.
+
+## Type Traits
+
+Type transformation functions introduced to `type_traits` C++11 were implemented with nested `typedefs` instead of alias declarations. In C++14, `_t` versions using alias declarations are added to STL.
+
+```c++
+// C++11
+tempalte<typename T>
+class MyClass {
+  typename std::add_lvalue_reference<T>::type t_ref;
+};
+
+// C++14
+tempalte<typename T>
+class MyClass {
+  std::add_lvalue_reference_t<T> t_ref;
+};
+```
--- cpp/effective_cpp/items/item_9/item_9.cpp
+++ cpp/effective_cpp/items/item_9/item_9.cpp
@@ -1,4 +1,53 @@
-#include <iostream>
+#include <string>
+#include <list>
+#include <type_traits>
+
+// Alias declaration version is easier to read:
+// Both FP1 and FP2 are pointers to functions that takes an int
+// and string argument then returns nothing.
+using FP1 = void (*)(int, const std::string&);
+typedef void (*FP2)(int, const std::string&);
+
+// Custom allocator
+template<typename T>
+class MyAlloc {};
+
+// Templatized alias declaration
+template<typename T>
+using MyAllocList1 = std::list<T, MyAlloc<T>>;
+
+// Nested typedef
+template<typename T>
+struct MyAllocList2 {
+  typedef std::list<T, MyAlloc<T>> type;
+};
+
+// Using these aliases:
+
+template<typename T>
+class Widget1 {
+ private:
+  MyAllocList1<T> list;
+};
+
+template<typename T>
+class Widget2 {
+ private:
+  typename  MyAllocList2<T>::type list;
+};
+
+// Type traits
+template<typename T>
+class MyClass1 {
+ private:
+  std::add_lvalue_reference_t<T> val_ref;
+};
+
+template<typename T>
+class MyClass2 {
+ private:
+  typename std::add_lvalue_reference<T>::type val_ref;
+};
 
 int main(int argc, char **argv) {
   return 0;

48c47431cd2cc7a15224e262d7b9742b521a2d70
Shuyang Sun <shuyangsun10@gmail.com>
2022-09-18 12:17:19 -0400

    Allow item_[0-9]+_[0-9]+ names.

--- cpp/effective_cpp/run.sh
+++ cpp/effective_cpp/run.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd "$SCRIPT_DIR"/build
 for FILE in *; do
-    [[ "$FILE" =~ ^item_[0-9]+$ ]] && "./$FILE"
+    [[ "$FILE" =~ ^item_[0-9]+(_[0-9]+)*$ ]] && "./$FILE"
 done

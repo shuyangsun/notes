c444eef8efe762b66d572871f03c57453facb076
Shuyang Sun <shuyangsun10@gmail.com>
2022-10-15 17:59:51 -0400

    Add iterator for component.

--- software_engineering/design_patterns/patterns/p08_composite/component.cc
+++ software_engineering/design_patterns/patterns/p08_composite/component.cc
@@ -1,21 +1,35 @@
 #include "patterns/p08_composite/component.h"
 
-#include <utility>
 #include <sstream>
+#include <utility>
 
-[[nodiscard]] absl::Status pattern::IComponent::Add(std::unique_ptr<IComponent> component) {
+namespace pattern {
+
+[[nodiscard]] absl::Status IComponent::Add(
+    std::unique_ptr<IComponent> component) {
   uint64_t tmp;
   return this->Add(std::move(component), tmp);
 }
 
-[[nodiscard]] absl::Status pattern::IComponent::Add(std::unique_ptr<IComponent> component, uint64_t& component_id) {
+[[nodiscard]] absl::Status IComponent::Add(
+    std::unique_ptr<IComponent> component, uint64_t &component_id) {
   return absl::AbortedError("Cannot add component to leaf component.");
 }
 
-absl::Status pattern::IComponent::RemoveById(uint64_t component_id) {
+absl::Status IComponent::RemoveById(uint64_t component_id) {
   return absl::AbortedError("Cannot remove component to leaf component.");
 }
 
-absl::Status pattern::IComponent::RemoveByFilter(std::function<bool(const IComponent &)> predicate) {
+absl::Status IComponent::RemoveByFilter(
+    std::function<bool(const IComponent &)> predicate) {
   return absl::AbortedError("Cannot remove component to leaf component.");
 }
+
+std::unique_ptr<IIterator<const IComponent &>> IComponent::Iter() {
+  return nullptr;
+}
+std::unique_ptr<IIterator<std::shared_ptr<IComponent>>> IComponent::IterMut() {
+  return nullptr;
+}
+
+}  // namespace pattern
--- software_engineering/design_patterns/patterns/p08_composite/component.h
+++ software_engineering/design_patterns/patterns/p08_composite/component.h
@@ -1,11 +1,12 @@
 #ifndef DESIGN_PATTERNS_PATTERNS_P08_COMPOSITE_COMPONENT_H_
 #define DESIGN_PATTERNS_PATTERNS_P08_COMPOSITE_COMPONENT_H_
 
-#include <memory>
-#include <functional>
 #include <cstdint>
+#include <functional>
+#include <memory>
 
 #include "absl/status/status.h"
+#include "patterns/p16_iterator/iterator.h"
 
 namespace pattern {
 
@@ -14,9 +15,13 @@ class IComponent {
   virtual ~IComponent() = default;
 
   [[nodiscard]] virtual absl::Status Add(std::unique_ptr<IComponent> component);
-  [[nodiscard]] virtual absl::Status Add(std::unique_ptr<IComponent> component, uint64_t& component_id);
+  [[nodiscard]] virtual absl::Status Add(std::unique_ptr<IComponent> component,
+                                         uint64_t& component_id);
   [[nodiscard]] virtual absl::Status RemoveById(uint64_t component_id);
-  [[nodiscard]] virtual absl::Status RemoveByFilter(std::function<bool (const IComponent&)> predicate);
+  [[nodiscard]] virtual absl::Status RemoveByFilter(
+      std::function<bool(const IComponent&)> predicate);
+  [[nodiscard]] virtual std::unique_ptr<IIterator<const IComponent&>> Iter();
+  [[nodiscard]] virtual std::unique_ptr<std::shared_ptr<IComponent>> IterMut();
 };
 
 }  // namespace pattern

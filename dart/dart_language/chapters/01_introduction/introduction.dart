143f5e01fb78983f17cffb498b75d0dd1e1ddfed
Shuyang Sun <shuyangsun10@gmail.com>
2024-04-23 20:29:44 -0400

    Dart notes change.

--- dart/dart_language/chapters/01_introduction/introduction.dart
+++ dart/dart_language/chapters/01_introduction/introduction.dart
@@ -1,7 +1,21 @@
 var flyByObjects = ['jupyter', 'plane'];
 
-void main() {
+// Map<String, Object>
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
+
+Map<String, String> audio = {'plane': '//path/to/plane.wav'};
+
+int printContains(List<String> arr, String str) {
+  int res = 0;
   flyByObjects
       .where((obj) => obj.contains('y'))
       .forEach((obj) => print('+++ ${obj} +++'));
+  return res;
+}
+
+void main() {
+  printContains(flyByObjects, 'y');
 }

a6b142aa299da0b709499316425bf6a697b683b3
Shuyang Sun <shuyangsun10@gmail.com>
2025-07-09 20:26:38 -0400

    Move client to client-web directory.

---
+++ protocols/oauth_2_in_action/client-web/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello, OAuth client!</h1>
+      <button>Get Prompts</button>
+    </>
+  );
+}
+
+export default App;
--- protocols/oauth_2_in_action/client/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import './App.css';
-
-function App() {
-  return <h1>Hello, OAuth client!</h1>;
-}
-
-export default App;

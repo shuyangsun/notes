f179f8aa29111ac5749558ecdb6687a6158177ab
Shuyang Sun <shuyangsun10@gmail.com>
2025-07-14 07:54:57 -0400

    Move components folder out of api folder.

--- protocols/oauth_2_in_action/client/src/app/page.tsx
+++ protocols/oauth_2_in_action/client/src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Main } from './api/components/Main/Main';
-import ServerInfo from './api/components/ServerInfo/ServerInfo';
+import { Main } from './components/Main/Main';
+import ServerInfo from './components/ServerInfo/ServerInfo';
 
 export default function Home() {
   return (

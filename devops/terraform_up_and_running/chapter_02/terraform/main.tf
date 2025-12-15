a96a2a1a731638482458dbf948955f18477fcd9d
Shuyang Sun <shuyangsun10@gmail.com>
2025-12-15 17:38:16 -0500

    First instance deployment successful.

--- devops/terraform_up_and_running/chapter_02/terraform/main.tf
+++ devops/terraform_up_and_running/chapter_02/terraform/main.tf
@@ -2,11 +2,43 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0ecb62995f68bb549"
-  instance_type = "t2.nano"
+  ami                    = "ami-0ecb62995f68bb549"
+  instance_type          = "t2.nano"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   tags = {
     Name = "terraform-example"
   }
+
+  user_data_replace_on_change = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, world" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+}
+
+output "public_uri" {
+  value       = "http://${aws_instance.example.public_ip}:8080"
+  description = "The public URI to access the webpage"
 }

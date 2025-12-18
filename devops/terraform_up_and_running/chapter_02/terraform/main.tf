371dd0285d892ccaf3f67096564b8c5cac6252f2
Shuyang Sun <shuyangsun10@gmail.com>
2025-12-18 17:20:44 -0500

    Terraform AWS autoscaling group example.

--- devops/terraform_up_and_running/chapter_02/terraform/main.tf
+++ devops/terraform_up_and_running/chapter_02/terraform/main.tf
@@ -2,6 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# ------------------------------- Networking -----------------------------------
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 variable "server_port" {
   description = "The port number to receive HTTP requests."
   type        = number
@@ -26,26 +39,128 @@ resource "aws_security_group" "instance" {
   }
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0ecb62995f68bb549"
-  instance_type          = "t2.nano"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+# ------------------------------ Load Balancer ---------------------------------
 
-  tags = {
-    Name = "terraform-example"
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  user_data_replace_on_change = true
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-  user_data = <<-EOF
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+
+# ------------------------------ Launch Template ---------------------------------
+
+resource "aws_launch_template" "example" {
+  name_prefix   = "terraform-example-"
+  image_id      = "ami-0ecb62995f68bb549"
+  instance_type = "t2.nano"
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = base64encode(<<-EOF
               #!/bin/bash
               echo "Hello, world" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# ---------------------------- Autoscaling Group -------------------------------
+
+resource "aws_autoscaling_group" "example" {
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
+
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 4
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
 }
 
 output "public_uri" {
   description = "The public URI to access the webpage."
   # Use `curl $(tf output public_uri | tr -d '"')` to test.
-  value = "http://${aws_instance.example.public_ip}:${var.server_port}"
+  value = "http://${aws_lb.example.dns_name}"
 }

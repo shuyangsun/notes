04214963c288869bbed77521806cfae30a1db8a2
Shuyang Sun <shuyangsun10@gmail.com>
2025-12-22 16:45:27 -0500

    Switch to new aws security group ingress / egress resources, and allow icmp.

--- devops/terraform_up_and_running/chapter_02/01_single_instance/main.tf
+++ devops/terraform_up_and_running/chapter_02/01_single_instance/main.tf
@@ -9,6 +9,10 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
+
 variable "port" {
   description = "Port to receive incoming traffic."
   type        = number
@@ -22,24 +26,45 @@ variable "instance_name" {
 }
 
 resource "aws_security_group" "instance" {
+  name        = "ec2-security-group"
+  description = "Firewall for the EC2 instance."
+
   tags = {
     Name = "${var.instance_name}-security-group"
   }
-
-  ingress {
-    from_port   = var.port
-    to_port     = var.port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ping" {
+  description = "Allow ping on EC2 instance."
+
+  security_group_id = aws_security_group.instance.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "icmp"
+  from_port   = -1
+  to_port     = -1
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_inbound" {
+  description = "Allow inbound traffic to server port."
+
+  security_group_id = aws_security_group.instance.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port   = var.port
+  to_port     = var.port
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_outbound" {
+  description = "Allow outbound traffic."
+
+  security_group_id = aws_security_group.instance.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
 
 resource "aws_instance" "example" {
   ami           = "ami-0ecb62995f68bb549"

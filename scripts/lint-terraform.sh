58c2591c6ca0eb79b8ab3955ba79c14edb3fef8c
Shuyang Sun <shuyangsun10@gmail.com>
2026-01-03 20:39:53 -0500

    Improve tflint script performance.

--- scripts/lint-terraform.sh
+++ scripts/lint-terraform.sh
@@ -11,11 +11,11 @@ DIRS=$(for file in $FILES; do dirname "$file"; done | sort -u)
 for DIR in $DIRS; do
   # Check if directory contains .tf files
   if ls "$DIR"/*.tf 1> /dev/null 2>&1; then
-    echo "Initializing $DIR"
-    terraform -chdir="$DIR" init -backend=false -input=false
-
-    echo "Validating $DIR"
-    terraform -chdir="$DIR" validate
+    # Only init if .terraform doesn't exist
+    if [ ! -d "$DIR/.terraform" ]; then
+      echo "Initializing $DIR"
+      terraform -chdir="$DIR" init -backend=false -input=false
+    fi
 
     echo "Linting $DIR"
     tflint --chdir="$DIR"

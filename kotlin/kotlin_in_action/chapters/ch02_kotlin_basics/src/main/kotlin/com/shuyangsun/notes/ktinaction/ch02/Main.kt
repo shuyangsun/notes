5c3ab472587fc3c2445d7de3e44413cefe46f31c
Shuyang Sun <shuyangsun10@gmail.com>
2026-06-30 13:18:41 -0400

    Add Kotlin in Action Chapter 2.1: Functions and variables

--- kotlin/kotlin_in_action/chapters/ch02_kotlin_basics/src/main/kotlin/com/shuyangsun/notes/ktinaction/ch02/Main.kt
+++ kotlin/kotlin_in_action/chapters/ch02_kotlin_basics/src/main/kotlin/com/shuyangsun/notes/ktinaction/ch02/Main.kt
@@ -1,5 +1,13 @@
 package com.shuyangsun.notes.ktinaction.ch02
 
+import com.shuyangsun.notes.ktinaction.ch02.util.max
+
 fun main() {
-  println("Hello, world!")
+  runSection1()
+}
+
+fun runSection1() {
+  val a = 3
+  val b = 5
+  println("The maximum value between $a and $b is ${max(3, 5)}.")
 }
---
+++ kotlin/kotlin_in_action/chapters/ch02_kotlin_basics/src/main/kotlin/com/shuyangsun/notes/ktinaction/ch02/util/Util.kt
@@ -0,0 +1,6 @@
+package com.shuyangsun.notes.ktinaction.ch02.util
+
+/**
+ * Returns the maximum of two integers.
+ */
+fun max(a: Int, b: Int) = if (a > b) a else b

dfe325c41f4241cf184d89218b4261c1fa730689
Shuyang Sun <shuyangsun10@gmail.com>
2026-06-30 13:48:20 -0400

    Add Kotlin in Action Chapter 2.2.3: add geometry

--- kotlin/kotlin_in_action/chapters/ch02_kotlin_basics/src/main/kotlin/com/shuyangsun/notes/ktinaction/ch02/Main.kt
+++ kotlin/kotlin_in_action/chapters/ch02_kotlin_basics/src/main/kotlin/com/shuyangsun/notes/ktinaction/ch02/Main.kt
@@ -2,6 +2,7 @@ package com.shuyangsun.notes.ktinaction.ch02
 
 import com.shuyangsun.notes.ktinaction.ch02.sec1.max
 import com.shuyangsun.notes.ktinaction.ch02.sec2.Person
+import com.shuyangsun.notes.ktinaction.ch02.sec2.Rectangle
 
 fun main() {
   runSection1()
@@ -18,4 +19,13 @@ fun runSection2() {
   val person = Person("Shuyang", false)
   person.isStudent = true
   println("$person")
+
+  println()
+
+  val rect1 = Rectangle(3u, 5u)
+  val rect2 = Rectangle(0u, 2u)
+  println("rect1 is ${if (rect1.isSquare) "" else "not "}square.")
+  if (!rect2.hasArea) {
+    println("rect2 does not have an area.")
+  }
 }

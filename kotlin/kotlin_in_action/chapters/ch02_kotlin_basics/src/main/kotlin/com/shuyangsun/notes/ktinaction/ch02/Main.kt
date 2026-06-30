f957f1b10fb188598b48845ec28a1a354cfffdb5
Shuyang Sun <shuyangsun10@gmail.com>
2026-06-30 13:20:39 -0400

    Add Kotlin in Action Chapter 2.3: Enums and when

--- kotlin/kotlin_in_action/chapters/ch02_kotlin_basics/src/main/kotlin/com/shuyangsun/notes/ktinaction/ch02/Main.kt
+++ kotlin/kotlin_in_action/chapters/ch02_kotlin_basics/src/main/kotlin/com/shuyangsun/notes/ktinaction/ch02/Main.kt
@@ -1,12 +1,15 @@
 package com.shuyangsun.notes.ktinaction.ch02
 
-import com.shuyangsun.notes.ktinaction.ch02.sec1.max
-import com.shuyangsun.notes.ktinaction.ch02.sec2.Person
-import com.shuyangsun.notes.ktinaction.ch02.sec2.Rectangle
+import com.shuyangsun.notes.ktinaction.ch02.sec1.*
+import com.shuyangsun.notes.ktinaction.ch02.sec2.*
+import com.shuyangsun.notes.ktinaction.ch02.sec3.*
 
 fun main() {
   runSection1()
+  println()
   runSection2()
+  println()
+  runSection3()
 }
 
 fun runSection1() {
@@ -28,4 +31,25 @@ fun runSection2() {
   if (!rect2.hasArea) {
     println("rect2 does not have an area.")
   }
+}
+
+fun runSection3() {
+  val color = Color.RED
+  color.printColor()
+  println("$color's mnemonic is \"${getMnemonic(color)}\", it is ${getWarmth(color)}.")
+
+  val c1 = Color.RED
+  val c2 = Color.YELLOW
+  val c3 = Color.BLUE
+  println("The mix of $c1 and $c2 is ${mixOptimized(c1, c2).getOrNull()}.")
+  println("The mix of $c1 and $c3 is ${mix(c1, c3).getOrNull()}.")
+
+  println()
+
+  val a = Num(3);
+  val b = Num(5);
+  val c = Num(2);
+  val expr1 = Sum(a, b)
+  println("expr1 = $expr1")
+  println("The sum of $a, $b, and $c is ${eval(Sum(expr1, c)).getOrThrow()}.")
 }

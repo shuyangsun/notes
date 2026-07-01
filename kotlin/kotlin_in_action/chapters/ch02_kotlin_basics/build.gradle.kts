d47ea1654904e92a2f6c77288940f5cdb503659d
Shuyang Sun <shuyangsun10@gmail.com>
2026-07-01 09:33:50 -0400

    Add ktfmt

--- kotlin/kotlin_in_action/chapters/ch02_kotlin_basics/build.gradle.kts
+++ kotlin/kotlin_in_action/chapters/ch02_kotlin_basics/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     kotlin("jvm") version "2.3.21"
+    id("com.diffplug.spotless") version "8.8.0"
 }
 
 group = "com.shuyangsun.notes.ktinaction.ch02"
+
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -15,6 +17,18 @@ dependencies {
 
 kotlin {
     jvmToolchain(25)
+}
+
+spotless {
+    kotlin {
+        target("../**/*.kt")
+        ktfmt("0.64").kotlinlangStyle()
+    }
+
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktfmt("0.64").kotlinlangStyle()
+    }
 }
 
 tasks.test {
